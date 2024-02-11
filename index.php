75436e009ebbced30b9420fd7b536193ba40e0e8
CarrotAlba <125712945+CarrotAlba@users.noreply.github.com>
2024-02-11 13:43:28 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html >
+    <body>
+<?php
+$dollar = 81.2;
+$ruble = 0.07;
+
+$dollars = 500;
+$rubles = $dollars*$dollar;
+$yuans = $rubles*$ruble;
+
+echo "1.00 USD = ";
+echo $yuans;
+echo " CNY";
+?>
+  </body>
+</html>
